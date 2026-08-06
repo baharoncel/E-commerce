aab95a3b1bab2b84b4f00c541985e15fade94c5e
Bahar <bahar@example.com>
2026-08-06 17:32:50 +0300

    fix: include media in STATICFILES_DIRS so WhiteNoise serves all user media images on Render

--- marketplace_project/settings.py
+++ marketplace_project/settings.py
@@ -135,9 +135,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+    BASE_DIR / 'media',
+]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
